7e67c656eeb41ea74e1476809f0028196002363b
eduar <chanaga250@gmail.com>
2024-04-11 08:49:37 -0500

    feat: :sparkles: fahrenheit to celsius
    se agrego el primero ejericicio a el archivo script.js

---
+++ Dia 1/js/script.js
@@ -0,0 +1,12 @@
+// ** Celsius a fahrenheit **
+function cambio(celsius) {
+fahrenheit=((9*celsius)/5)+32;
+return fahrenheit;
+}
+
+celsius = 10;
+
+console.log(cambio(celsius));
+
+
+//** Desarrollado por: Eduar Damian Chanaga Gonzalez */
